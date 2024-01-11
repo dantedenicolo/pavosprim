6dd530d6eef345aa863d5319ad37ef1b56d64c25
Dante <denicolodante@gmail.com>
2024-01-11 20:05:25 -0300

    speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
 		<NextUIProvider>
 			<Analytics />
+			<SpeedInsights />
 			<App />
 		</NextUIProvider>
 	</BrowserRouter>
